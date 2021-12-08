fad2b92c15cc65ccdc63c10c77f706c6ce4d6f33
Liana Lixandru <liana.lixandru@oracle.com>
2021-12-08 15:31:05 +0200

    Implemented search across all regions

--- func.py
+++ func.py
@@ -8,6 +8,8 @@ import json
 import oci
 import requests
 from fdk import response
+import os
+import base64
 
 
 def handler(ctx, data: io.BytesIO = None):
@@ -15,19 +17,47 @@ def handler(ctx, data: io.BytesIO = None):
         body = json.loads(data.getvalue())
     except Exception:
         raise Exception()
-    resp = get_usage(body)
+    resp = search(body)
     return response.Response(
         ctx,
-        response_data=json.dumps(resp.json()),
+        response_data=json.dumps(resp),
         headers={"Content-Type": "application/json"}
     )
 
 
-def get_usage(body):
+def search(body):
     signer = oci.auth.signers.get_resource_principals_signer()
     try:
-        endpoint = 'https://query.eu-frankfurt-1.oci.oraclecloud.com/20180409/resources'
-        output = requests.post(endpoint, json=body, auth=signer)
+
+        # Read resource principal data and get tenancy OCID
+        rpst = open(os.environ.get('OCI_RESOURCE_PRINCIPAL_RPST'), "r")
+        env = rpst.read()
+        payload = env.split('.')[1]
+        base64_bytes = payload.encode('ascii')
+        message_bytes = base64.b64decode(base64_bytes)
+        payloadDecoded = message_bytes.decode('ascii')
+
+        tenancy = json.loads(payloadDecoded)['res_tenant']
+
+        # Construct the region subscription URL
+        endpoint_regionSub = 'https://identity.eu-frankfurt-1.oraclecloud.com/20160918/tenancies/' + \
+            tenancy + '/regionSubscriptions'
+
+        # Get subscribed regions for the tenancy
+        regionSub = requests.get(endpoint_regionSub, auth=signer)
+        regionSub = json.loads(json.dumps(regionSub.json()))
+
+        # Call the Search Service API and get the query data
+        result = []
+        for region in regionSub:
+            if region['status'] == 'READY':
+                endpoint = 'https://query.' + \
+                    region['regionName'] + \
+                    '.oci.oraclecloud.com/20180409/resources'
+                output = requests.post(endpoint, json=body, auth=signer)
+                if output:
+                    output = json.loads(json.dumps(output.json()))
+                    result = result + output['items']
     except Exception as e:
-        output = "Failed: " + str(e.message)
-    return output
+        result = "Failed: " + str(e)
+    return result
